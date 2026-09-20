725ce6011bec8c93dd6346189a1b1d6d41395545
Mudassir <mudassir.code.in@gmail.com>
2026-09-20 15:40:53 +0530

    login-api

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,138 @@
+import jwt from 'jsonwebtoken';
+import redisClient from '../config/redis.config.js';
+import type { Request, Response } from 'express';
+import { userModel } from '../models/user.model.js';
+import crypto from 'crypto';
+
+
+
+
+export async function login(req: Request, res: Response): Promise<any> {
+    try {
+        const { name, email, avatar } = req.body;
+
+
+        const user = await userModel.findOne({ email });
+
+
+        // if there is a user in the database
+        if (user) {
+
+            const sessionData = {
+                userId: user._id,
+                userAgent: req.headers['user-agent'],
+                ip: req.ip
+            }
+
+            const sessionId = crypto.randomUUID();
+            const redisKey: string = `session:${user._id}:${sessionId}`;
+
+            await redisClient.set(redisKey, JSON.stringify(sessionData), {
+                EX: 604800
+            });
+
+
+            const refreshToken = jwt.sign({
+                userId: user._id,
+                sessionId: sessionId
+            }, process.env.JWT_REFRESH_SECRET!, {expiresIn: '7d'});
+
+
+            const accessToken = jwt.sign({
+                userId: user._id,
+                sessionId: sessionId
+            }, process.env.JWT_ACCESS_SECRET!, {expiresIn: '15m'});
+
+
+            res.cookie('refreshToken', refreshToken, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'none'
+            });
+
+            res.cookie('accessToken', accessToken, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'none'
+            });
+
+            res.status(200).json({
+                success: true,
+                message: 'User login successfully',
+                user: {
+                    name: user.name,
+                    email: user.email,
+                    avatar: user.avatar
+                }
+            });
+
+        }
+
+
+        //if there is no user in the database
+        const newUser = await userModel.create({
+            name,
+            email,
+            avatar 
+        });
+
+
+        const sessionData = {
+                userId: newUser._id,
+                userAgent: req.headers['user-agent'],
+                ip: req.ip
+            }
+
+            const sessionId = crypto.randomUUID();
+            const redisKey: string = `session:${newUser._id}:${sessionId}`;
+
+            await redisClient.set(redisKey, JSON.stringify(sessionData), {
+                EX: 604800
+            });
+
+
+            const refreshToken = jwt.sign({
+                userId: newUser._id,
+                sessionId: sessionId
+            }, process.env.JWT_REFRESH_SECRET!, {expiresIn: '7d'});
+
+
+            const accessToken = jwt.sign({
+                userId: newUser._id,
+                sessionId: sessionId
+            }, process.env.JWT_ACCESS_SECRET!, {expiresIn: '15m'});
+
+
+            res.cookie('refreshToken', refreshToken, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'none'
+            });
+
+            res.cookie('accessToken', accessToken, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'none'
+            });
+
+            res.status(201).json({
+                success: true,
+                message: 'User Registerd successfully',
+                user: {
+                    name: newUser.name,
+                    email: newUser.email,
+                    avatar: newUser.avatar
+                }
+            });
+
+
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: 'Internal server error'
+        })
+    }
+}
---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { login } from '../controllers/auth.controller.js';
+
+
+export const authRouter = express.Router();
+
+
+authRouter.post('/login', login);
+
+
+
+
